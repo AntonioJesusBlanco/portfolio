ee0553cffa95d3914ed5046d710b6cb239c98302
AntonioJesusBlanco <ablagui1506@g.educaand.es>
2025-06-10 20:18:53 +0200

    ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel'; // o "vercel/edge" si prefieres
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server', // importante para que funcione con Vercel Serverless
+  adapter: vercel(),
+});
