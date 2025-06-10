ac6cf5e6c1ffe8fc3c32c0912f6fdb91c4638b07
AntonioJesusBlanco <ablagui1506@g.educaand.es>
2025-06-10 20:24:21 +0200

    ready to deploy

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DZj9HY18.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel'; // o "vercel/edge" si prefieres
 
 export default defineConfig({
-  output: 'server', // importante para que funcione con Vercel Serverless
+  output: 'static', 
   adapter: vercel(),
 });
