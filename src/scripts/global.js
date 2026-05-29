381cb2f88dd7508af99ae7d140adb7b1d02daebc
AntonioJesusBlanco <ablagui1506@g.educaand.es>
2026-05-29 17:25:47 +0200

    Cambios generales de aspecto en el resto de la aplicacion web

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -8,6 +8,10 @@ const skillDivs = document.querySelectorAll('.skills-grid div');
 const backToTopBtn = document.getElementById('back-to-top');
 const lightGradients = document.querySelector('.light-gradients');
 const darkGradients = document.querySelector('.dark-gradients');
+//Poner de primeras el tema oscuro
+document.addEventListener('DOMContentLoaded', () => {
+  applyDarkMode();
+});
 
 // Verificar tema guardado
 const savedTheme = localStorage.getItem('theme') || 'light';
